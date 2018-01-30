e1c6b9da5a5d6fb57bc6ff3c33115895ac1dcbca
Niyaz <mukhamedya.n@gmail.com>
2018-01-30 17:39:37 +0600

    finished exam WITHOUT saving to file or JSON

--- SecurityEntrance/CheckAbsent.xaml.cs
+++ SecurityEntrance/CheckAbsent.xaml.cs
@@ -20,6 +20,10 @@ namespace SecurityEntrance
     /// </summary>
     public partial class CheckAbsent : Window
     {
+
+        public ObservableCollection<DayCheckedInfo> daysChecked { get; set; }
+        public ObservableCollection<Employee> defaultListEmployees { get; set; }
+
         public CheckAbsent()
         {
             InitializeComponent();
@@ -28,27 +32,85 @@ namespace SecurityEntrance
         public CheckAbsent(ObservableCollection<Employee> listEmployeeFromMainWindows)
         {
             InitializeComponent();
-            List<InfoCheckEmployee> infoData = new List<InfoCheckEmployee>();
+            defaultListEmployees = new ObservableCollection<Employee>();
+            defaultListEmployees = listEmployeeFromMainWindows;
+            DayCheckedInfo infoDataToday = new DayCheckedInfo();
             foreach (Employee e in listEmployeeFromMainWindows)
             {
                 InfoCheckEmployee temp = new InfoCheckEmployee();
                 temp.Id = e.Id;
                 temp.Name = e.Name;
                 temp.Position = e.Position;
-                infoData.Add(temp);
+                infoDataToday.infoEmployees.Add(temp);
+            }
+            infoDataToday.dayChecked = DateTime.Today;
+            daysChecked = new ObservableCollection<DayCheckedInfo>();
+            daysChecked.Add(infoDataToday);
+
+            employeeList.ItemsSource = daysChecked.Last().infoEmployees;
+            checkCalendar.SelectedDate = DateTime.Today;
+        }
+        
+        
+
+        private void checkCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            bool hasChangedList = false;
+
+            for (int i = 0; i < daysChecked.Count; i++)
+            {
+                if (daysChecked[i].dayChecked == checkCalendar.SelectedDate)
+                {
+                    hasChangedList = true;
+                }
             }
 
-            employeeList.ItemsSource = infoData;
+            if (!hasChangedList)
+            {
+                DayCheckedInfo infoDataNew = new DayCheckedInfo();
+
+                foreach (Employee employeeTempNew in defaultListEmployees)
+                {
+                    InfoCheckEmployee temp = new InfoCheckEmployee();
+                    temp.Id = employeeTempNew.Id;
+                    temp.Name = employeeTempNew.Name;
+                    temp.Position = employeeTempNew.Position;
+                    infoDataNew.infoEmployees.Add(temp);
+                }
+                infoDataNew.dayChecked = (DateTime)checkCalendar.SelectedDate;
+                daysChecked.Add(infoDataNew);
+                employeeList.ItemsSource = daysChecked.Last().infoEmployees;
+            }
+            else
+            {
+                for (int i = 0; i < daysChecked.Count; i++)
+                {
+                    if (checkCalendar.SelectedDate == daysChecked[i].dayChecked)
+                    {
+                        employeeList.ItemsSource = daysChecked[i].infoEmployees;
+                    }
+                }
+            }
 
         }
     }
 
+    public class DayCheckedInfo
+    {
+        public List<InfoCheckEmployee> infoEmployees { get; set; }
+        public DateTime dayChecked { get; set; }
+        public DayCheckedInfo()
+        {
+            infoEmployees = new List<InfoCheckEmployee>();
+        }
+    }
+}
 
-    public class InfoCheckEmployee
+public class InfoCheckEmployee
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Position { get; set; }
         public bool IsEntered { get; set; }
     }
-}
+
